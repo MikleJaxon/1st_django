1b58e221e6f33e298a8bbb93b350e7d0b458a8d9
Mikle Jaxon <odegeto1337@gmail.com>
2024-01-12 22:27:53 +0300

    adding pictures

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -13,6 +13,7 @@ class Product(models.Model):
     name = models.CharField(max_length = 100)
     price = models.IntegerField()
     description = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, upload_to="images")
     
     def __str__(self):
         return self.name
