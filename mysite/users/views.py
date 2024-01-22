1d012ab84df123a865f6d9922d6fc6474770b87e
Mikle Jaxon <odegeto1337@gmail.com>
2024-01-22 16:43:36 +0300

    backup

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from .forms import NewUserForm
 
 def register(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            
     form = NewUserForm()
     context ={
         'form':form
