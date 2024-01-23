44b0b092f7828bd5e5f2ef41348d06777ff3e78f
Mikle Jaxon <odegeto1337@gmail.com>
2024-01-23 17:01:49 +0300

    profile model

--- mysite/users/admin.py
+++ mysite/users/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Profie
 
-# Register your models here.
+admin.site.register(Profie)
--- mysite/users/models.py
+++ mysite/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
+class Profie(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(blank=True, upload_to='profiel_images')
+    contact_number = models.CharField(max_length=20, default="+7123456789")
+    
+    def __str__(self):
+        return self.user.username
