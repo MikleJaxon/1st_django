ee223e12d6b32e6d459f062690790809e3bfaa96
Михаил Ледевич <odegeto1337@gmail.com>
2024-02-06 18:45:06 +0300

    asd

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 
 from .models import Product
@@ -20,13 +21,16 @@ def indexItem(request, prod_id):
     }
     return render(request, "myapp/detail.html", context=context)
 
+@login_required
 def add_item(request):
     if request.method == "POST":
         name = request.POST.get("name")
         price = request.POST.get("price")
+        
         description = request.POST.get("description")
         image = request.FILES["upload"]
-        item = Product(name=name, price=price, description=description, image=image)
+        seller = request.user
+        item = Product(name=name, price=price, description=description, image=image, seller=seller)
         item.save()
     return render(request, "myapp/additem.html")
 
