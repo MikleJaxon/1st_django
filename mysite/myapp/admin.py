6e50e74018fe84eb16cc6bd533aacc727a02d136
Михаил Ледевич <odegeto1337@gmail.com>
2024-02-08 15:01:39 +0300

    admin pane

--- mysite/myapp/admin.py
+++ mysite/myapp/admin.py
@@ -2,6 +2,22 @@ from django.contrib import admin
 
 from .models import Product
 
-admin.site.register(Product)
 
+
+admin.site.site_header = "Admin Panel"
+
+admin.site.site_title = "Admin panel/phone shop"
+
+admin.site.index_title = "Admin panel"
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'description')
+    search_fields = ('name',)
+    list_editable = ('price','description')
+    actions = ('make_zero',)
+
+    def make_zero(self, request, queryset):
+        queryset.update(price = 0)
+
+admin.site.register(Product, ProductAdmin)
 
